2482efd808be868bf21b52122d11cd6e7ab3cf72
kelvinmaxwell <kelvinmaxwell82@gmail.com>
2020-02-10 10:08:08 +0300

    changes

---
+++ dist/js/main.js
@@ -0,0 +1,31 @@
+//select Dom items
+
+const menubtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menunav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+//set Intial state of menu
+let showMenu = false;
+menubtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menubtn.classList.add("close");
+    menu.classList.add("show");
+    menunav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+    //set menu state;
+    showMenu = true;
+  } else {
+    menubtn.classList.remove("close");
+    menu.classList.remove("show");
+    menunav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+    //set menu state;
+    showMenu = false;
+  }
+}
